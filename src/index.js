ed8720f211c9fdbf4952ea4ba169e19f150b7258
shipulinia <shipulinia@lad24.ru>
2019-09-23 09:14:11 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  const result = [];
+  first
+    .split("")
+    .reverse()
+    .forEach((item1, i) =>
+      second
+        .split("")
+        .reverse()
+        .forEach(
+          (item2, j) =>
+            (result[i + j] = result[i + j]
+              ? result[i + j] + item1 * item2
+              : item1 * item2)
+        )
+    );
+  for (let i = 0; i < result.length; i++) {
+    const temp = result[i] % 10;
+    const add = Math.floor(result[i] / 10);
+    result[i] = temp;
+    if (result[i + 1]) result[i + 1] += add;
+    else if (add) result[i + 1] = add;
+  }
+
+  return result.reverse().join("");
+};
